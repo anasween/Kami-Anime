ab555bcb4697a90b305356d988109d3d43aa2ee9
beer <beer@archi>
2014-01-31 21:07:16 +0300

    Some changes in news view.

--- protected/views/news/_shortView.php
+++ protected/views/news/_shortView.php
@@ -1,67 +1,67 @@
 <div class="row post">
     <div class="col-md-12">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>
-                <a href="<?php echo $this->createUrl('news/view',array('id'=>$model->id)); ?>">
-                    <?php echo $model->title ?>
-                </a>
-                <?php
-                if (Yii::app()->user->can('news'))
-                {
-                    echo BSHtml::buttonDropdown(Yum::t('Actions'), 
+        <div class="row">
+            <div class="col-md-12">
+                <h1>
+                    <a href="<?php echo $this->createUrl('news/view',array('id'=>$model->id)); ?>">
+                        <?php echo $model->title ?>
+                    </a>
+                    <?php
+                    if (Yii::app()->user->can('news'))
+                    {
+                        echo BSHtml::buttonGroup(array(
                             array(
-                                BSHtml::dropDownHeader(Yum::t('Actions')),
-                                array(
-                                    'label' => Yum::t('Create'), 
-                                    'url' => array('news/create'),
-                                    'visible' => Yii::app()->user->can('news', 'create'),
-                                ),
-                                array(
-                                    'label' => Yum::t('Update'), 
-                                    'url' => array("news/update", "id"=>$model->id),
-                                    'visible' => Yii::app()->user->can('news', 'update'),
-                                ),
-                                array(
-                                    'label' => Yum::t('Delete'), 
-                                    'url'=>'#', 
-                                    'linkOptions'=>array(
-                                        'submit'=>array(
-                                            'delete',
-                                            'id'=>$model->id
-                                        ),
-                                        'confirm' => Yum::t('Are you sure to delete this item?'),
-                                    ),
-                                    'visible' => Yii::app()->user->can('news', 'delete'),
-                                ),
+                                'icon' => BSHtml::GLYPHICON_PLUS_SIGN, 
+                                'url' => array('news/create'),
+                                'visible' => Yii::app()->user->can('news', 'create'),
+                                'color' => BSHtml::BUTTON_COLOR_SUCCESS,
+                                'data-title' => Yum::t('Create'),
+                                'title' => '',
+                                'data-toggle' => 'tooltip'
                             ),
                             array(
-                                'color' => BSHtml::BUTTON_COLOR_PRIMARY
-                            )
-                        );
-                }
-                ?>
-            </h1>
-            
+                                'icon' => BSHtml::GLYPHICON_EDIT, 
+                                'url' => array("news/update", "id"=>$model->id),
+                                'visible' => Yii::app()->user->can('news', 'update'),
+                                'color' => BSHtml::BUTTON_COLOR_WARNING,
+                                'data-title' => Yum::t('Edit'),
+                                'title' => '',
+                                'data-toggle' => 'tooltip'
+                            ),
+                            array(
+                                'icon' => BSHtml::GLYPHICON_MINUS_SIGN, 
+                                'url'=>array('delete','id'=>$model->id),
+                                'visible' => Yii::app()->user->can('news', 'delete'),
+                                'color' => BSHtml::BUTTON_COLOR_DANGER,
+                                'data-title' => Yum::t('Delete'),
+                                'title' => '',
+                                'data-toggle' => 'tooltip'
+                            ),
+                        ), array(
+                            'type' => BSHtml::BUTTON_TYPE_LINK
+                        ));
+                    }
+                    ?>
+                </h1>
+            </div>
         </div>
-    </div>
-    <div class="row post_body">
-        <div class="col-md-12">
-            <?php echo $model->text; ?>
+        <div class="row post_body">
+            <div class="col-md-12">
+                <?php echo $model->text; ?>
+            </div>
         </div>
-    </div>
-    <div class="post_description">
-        <?php 
-        echo BSHtml::icon(BSHtml::GLYPHICON_EYE_OPEN) . ' '
-                . $model->views . ' '
-                . '<a href="' . $this->createUrl('user/admin/view',array('id'=>$model->autor->id)) . '">'
-                . BSHtml::icon(BSHtml::GLYPHICON_USER) . ' '
-                . $model->autor->username 
-                . '</a> '
-                . BSHtml::icon(BSHtml::GLYPHICON_CALENDAR) . ' '
-                . Yii::app()->dateFormatter->format('d MMMM yyyy HH:mm:ss', $model->create_Date) . ' '
-                . BSHtml::icon(BSHtml::GLYPHICON_COMMENT) . ' '
-                . count($model->comments); ?>
-    </div>
+        <div class="post_description">
+            <?php 
+            echo BSHtml::icon(BSHtml::GLYPHICON_EYE_OPEN) . ' '
+                    . $model->views . ' | '
+                    . '<a href="' . $this->createUrl('user/admin/view',array('id'=>$model->autor->id)) . '">'
+                    . BSHtml::icon(BSHtml::GLYPHICON_USER) . ' '
+                    . $model->autor->username 
+                    . '</a> | '
+                    . BSHtml::icon(BSHtml::GLYPHICON_CALENDAR) . ' '
+                    . Yii::app()->dateFormatter->format('d MMMM yyyy HH:mm:ss', $model->create_Date) . ' | '
+                    . BSHtml::icon(BSHtml::GLYPHICON_COMMENT) . ' '
+                    . count($model->comments); ?>
+        </div>
     </div>
 </div>
